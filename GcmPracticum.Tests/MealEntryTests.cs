12826c41a121048fdccfd4428e0d19347fbd5e5a
david foster <david_t_foster@live.com>
2015-09-18 08:20:28 -0500

    add Meal Entry structure

---
+++ GcmPracticum.Tests/MealEntryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace GcmPracticum.Tests
+{
+    public class MealEntryTests
+    {
+        [Fact]
+        public void SmokeTest()
+        {
+            var entry = new MealEntry
+            (
+                dishes: new string[] { "one", "two"},
+                allowedMultiple: new int[] { 1}
+            );
+
+            entry.Should().NotBeNull();
+            entry.Dishes.Count().Should().Be(2);
+            entry.AllowedMultiple.Count().Should().Be(1);
+        }
+    }
+}
